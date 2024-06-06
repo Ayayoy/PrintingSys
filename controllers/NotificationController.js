bf00141f13923b3db21d3b62cb06b15c24e422b2
karimezz22 <74823379+karimezz22@users.noreply.github.com>
2024-06-06 03:07:41 +0300

    updates

--- controllers/NotificationController.js
+++ controllers/NotificationController.js
@@ -3,6 +3,17 @@ const Notification = require("../models/notification");
 const getNotifications = async (req, res, next) => {
   try {
     const userId = req.params.userId;
+    const notifications = await Notification.find({ user_id: userId }).sort({ createdAt: -1 }).exec();
+
+    res.status(200).json({ message: 'Notifications fetched successfully', data: notifications });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getLastThreeNotifications = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
     const notifications = await Notification.find({ user_id: userId })
       .sort({ createdAt: -1 })
       .limit(3)
@@ -15,5 +26,6 @@ const getNotifications = async (req, res, next) => {
 };
 
 module.exports = {
-  getNotifications
+  getNotifications,
+  getLastThreeNotifications
 };
--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getNotifications } = require("../controllers/NotificationController");
+const { getNotifications, getLastThreeNotifications } = require("../controllers/NotificationController");
 const router = express.Router();
 
 router.get("/:userId", getNotifications);
+router.get("/getLastThree/:userId", getLastThreeNotifications);
 
 module.exports = router;
